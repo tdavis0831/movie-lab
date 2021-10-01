e027d3a1af662f9fad387aaf042792a5bfdb3049
Tasha Davis <tdavis0831@yahoo.com>
2021-10-01 04:01:44 +0000

    working on adding rating

--- crud.py
+++ crud.py
@@ -3,31 +3,59 @@
 from model import db, User, Movie, Rating, connect_to_db
 
 
-
 def create_user(email, password):
     """Create and return a new user."""
 
-    user = User(email=email password=password)
+    user = User(email=email, password=password)
 
     db.session.add(user)
     db.session.commit()
 
     return user
 
-def create_movie(title, overview, release_date, poster_path):
 
-    movie = Movie(title=title, 
-    overview=overview, 
-    release_date=release_date, 
-    poster_path=poster_path)
+def get_users():
+    """Return all users."""
+
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    """Return a user by primary key."""
+
+    return User.query.get(user_id)
+
+
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(
+        title=title,
+        overview=overview,
+        release_date=release_date,
+        poster_path=poster_path,
+    )
 
     db.session.add(movie)
     db.session.commit()
-    
+
     return movie
 
-def create_rating(user, movie, score):
 
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
+
+def get_movie_by_id(movie_id):
+    """Return a movie by primary key."""
+
+    return Movie.query.get(movie_id)
+
+
+def create_rating(user, movie, score):
+    """Create and return a new rating."""
 
     rating = Rating(user=user, movie=movie, score=score)
 
@@ -36,8 +64,12 @@ def create_rating(user, movie, score):
 
     return rating
 
+def get_user_by_email(email):
+
+        return User.query.filter(User.email == email).first()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from server import app
+
     connect_to_db(app)
--- server.py
+++ server.py
@@ -1,11 +1,109 @@
+
+
+
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db
+import crud
+
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
-# Replace this with routes and view functions!
+@app.route("/")
+def homepage():
+    """View homepage."""
+
+    return render_template("homepage.html")
+
+
+@app.route("/movies")
+def all_movies():
+    """View all movies."""
+
+    movies = crud.get_movies()
+
+    return render_template("all_movies.html", movies=movies)
+
+
+@app.route("/movies/<movie_id>")
+def show_movie(movie_id):
+    """Show details on a particular movie."""
+
+    movie = crud.get_movie_by_id(movie_id)
+
+    return render_template("movie_details.html", movie=movie)
+
+
+@app.route("/users", methods = ["POST"])
+def register_user():
+    """View all users."""
+
+    email = request.form.get("email") 
+
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+
+    if user:
+        flash("Email exists already, plese try logging in")
+    else:
+        crud.create_user(email, password)
+        flash("Account Created")
+
+    return redirect("/")
+
+@app.route('/rating', methods =["POST"])
+def create_rating():
+    
+    user = crud.get_user_by_email(email)
+    movie = crud.get_movie_by_id(movie_id)
+    rating = request.args.get()
+    crud.create_rating(user, movie, )
+    def create_rating(user, movie, score):
+
+    return render_template(movie_details.html)
+
+
+
+
+
+@app.route('/login', methods =["POST"])
+def login():
+    email = request.form.get("email") 
+
+    password = request.form.get("password")
+    user = crud.get_user_by_email(email)
+
+    if user and user.password ==password:
+        session["user_id"] = user.email
+        flash("You are logged in")
+    else:
+        flash("The email or password you entered was incorrect.")
+    return redirect("/")
+
+
+
+
+@app.route("/users/<user_id>")
+def show_user(user_id):
+    """Show details on a particular user."""
+
+    user = crud.get_user_by_id(user_id)
+        return render_template("user_details.html", user=user)
+
+
+
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
+
 
 
 if __name__ == "__main__":
